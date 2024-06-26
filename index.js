10e4f37ce45e019809636b4976b6552d57eb320d
Manish <reg42mi@gmail.com>
2024-06-26 04:51:35 -0400

    new test json update

--- index.js
+++ index.js
@@ -3,14 +3,47 @@ const express = require("express");
 
 const app = express();
 
-const port = 4000;
+const githubData = {
+  login: "meregmi",
+  id: 173387671,
+  node_id: "U_kgDOClWvlw",
+  avatar_url: "https://avatars.githubusercontent.com/u/173387671?v=4",
+  gravatar_id: "",
+  url: "https://api.github.com/users/meregmi",
+  html_url: "https://github.com/meregmi",
+  followers_url: "https://api.github.com/users/meregmi/followers",
+  following_url: "https://api.github.com/users/meregmi/following{/other_user}",
+  gists_url: "https://api.github.com/users/meregmi/gists{/gist_id}",
+  starred_url: "https://api.github.com/users/meregmi/starred{/owner}{/repo}",
+  subscriptions_url: "https://api.github.com/users/meregmi/subscriptions",
+  organizations_url: "https://api.github.com/users/meregmi/orgs",
+  repos_url: "https://api.github.com/users/meregmi/repos",
+  events_url: "https://api.github.com/users/meregmi/events{/privacy}",
+  received_events_url: "https://api.github.com/users/meregmi/received_events",
+  type: "User",
+  site_admin: false,
+  name: null,
+  company: null,
+  blog: "",
+  location: null,
+  email: null,
+  hireable: null,
+  bio: null,
+  twitter_username: null,
+  public_repos: 1,
+  public_gists: 0,
+  followers: 0,
+  following: 0,
+  created_at: "2024-06-20T15:49:04Z",
+  updated_at: "2024-06-20T16:09:14Z",
+};
 
 app.get("/", (req, res) => {
   res.send("hello world");
 });
 
 app.get("/test", (req, res) => {
-  res.send("Hello from test");
+  res.json(githubData);
 });
 
 app.get("/login", (req, res) => {
@@ -22,5 +55,5 @@ app.get("/service", (req, res) => {
 });
 
 app.listen(process.env.PORT, () => {
-  console.log(`Example app listen on port ${port}`);
+  console.log(`Example app listen on port ${process.env.PORT}`);
 });
